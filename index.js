97f6e5e9aebae61ee379152a8c30e11cf7b505b9
heisenberg550 <150204095@kku.edu.tr>
2023-03-28 02:01:52 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+
+function superbowlWin(record) {
+    const winRecord = record.find(x => x.result === "W");
+    if (winRecord) {
+      return winRecord.year;
+    }
+    return undefined;
+  }
